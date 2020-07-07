f72e6f16f9e68131e6f0bae7b5dcd643bd563921
Andrey Kozlov <yecli.github@gmail.com>
2020-07-07 16:23:55 +0500

    VP-3289: Remove redundant tab

--- src/VirtoCommerce.DynamicAssociationsModule.Web/Scripts/module.js
+++ src/VirtoCommerce.DynamicAssociationsModule.Web/Scripts/module.js
@@ -6,39 +6,8 @@ if (AppDependencies !== undefined) {
 }
 
 angular.module(moduleName, [])
-    .config(['$stateProvider', '$urlRouterProvider',
-        function ($stateProvider, $urlRouterProvider) {
-            $stateProvider
-                .state('workspace.virtoCommerceDynamicAssociationsModuleState', {
-                    url: '/virtoCommerce.dynamicAssociationsModule',
-                    templateUrl: '$(Platform)/Scripts/common/templates/home.tpl.html',
-                    controller: [
-                        '$scope', 'platformWebApp.bladeNavigationService', function ($scope, bladeNavigationService) {
-                            var newBlade = {
-                                id: 'blade1',
-                                controller: 'virtoCommerce.dynamicAssociationsModule.helloWorldController',
-                                template: 'Modules/$(virtoCommerce.dynamicAssociationsModule)/Scripts/blades/hello-world.html',
-                                isClosingDisabled: true
-                            };
-                            bladeNavigationService.showBlade(newBlade);
-                        }
-                    ]
-                });
-        }
-    ])
     .run(['platformWebApp.mainMenuService', 'platformWebApp.widgetService', '$state', 'virtoCommerce.marketingModule.marketingMenuItemService',
         function (mainMenuService, widgetService, $state, marketingMenuItemService) {
-            //Register module in main menu
-            var menuItem = {
-                path: 'browse/virtoCommerce.dynamicAssociationsModule',
-                icon: 'fa fa-cube',
-                title: 'VirtoCommerce.DynamicAssociationsModule',
-                priority: 100,
-                action: function () { $state.go('workspace.virtoCommerceDynamicAssociationsModuleState'); },
-                permission: 'virtoCommerceDynamicAssociationsModule:access'
-            };
-            mainMenuService.addMenuItem(menuItem);
-
             marketingMenuItemService.register({
                 id: '3',
                 name: 'Dynamic product associations',
