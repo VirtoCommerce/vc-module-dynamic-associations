2c15de024c926b102e28c1d44dc2d1daa9e7b7a3
Basil Kotov <basilkot@gmail.com>
2026-01-13 11:36:26 +0000

    use search store instead of query

--- src/VirtoCommerce.DynamicAssociationsModule.Web/Scripts/blades/mainParameters.js
+++ src/VirtoCommerce.DynamicAssociationsModule.Web/Scripts/blades/mainParameters.js
@@ -5,6 +5,7 @@ angular.module('virtoCommerce.dynamicAssociationsModule')
         blade.headIcon = 'fa fa-area-chart';
         var formScope;
         $scope.setForm = (form) => { formScope = form; };
+        $scope.storeDataSource = (criteria) => stores.search(criteria);
 
         $scope.isValid = function() {
             return formScope && formScope.$valid;
@@ -19,18 +20,17 @@ angular.module('virtoCommerce.dynamicAssociationsModule')
                 }
             });
 
-            stores.query({}, response => {
-                $scope.stores = response;
-                if (parentRefresh) {
-                    blade.parentBlade.refresh();
-                }
-                blade.isLoading = false;
-            });
+            if (parentRefresh) {
+                blade.parentBlade.refresh();
+            }
+            blade.isLoading = false;
 
             blade.currentEntity = angular.copy(blade.originalEntity);
         };
 
-        $scope.onStoreSelected = ($item) => blade.currentEntity.catalogId = $item.catalog;
+        $scope.onStoreSelected = (item) => {
+            blade.currentEntity.catalogId = item.catalog;
+        };
         
         // datepicker 
         $scope.datepickers = {
